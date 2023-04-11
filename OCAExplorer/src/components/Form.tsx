42e256fa17c3bb2e99b6742d2894ced06033ff8a
Jean-Christophe <Jean-Christophe.Drouin@mcn.gouv.qc.ca>
2023-04-11 09:55:44 -0400

    QC demo OCA Bundle
    Signed-off-by: Jean-Christophe <Jean-Christophe.Drouin@mcn.gouv.qc.ca>

--- OCAExplorer/src/components/Form.tsx
+++ OCAExplorer/src/components/Form.tsx
@@ -13,6 +13,9 @@ const urlMap = new Map(
   Object.entries({
     "XUxBrVSALWHLeycAUhrNr9:2:student_card:1.0":
       "https://raw.githubusercontent.com/bcgov/aries-oca-bundles/main/OCABundles/XUxBrVSALWHLeycAUhrNr9/XUxBrVSALWHLeycAUhrNr9%3A2%3Astudent_card%3A1.0/OCABundle.json",
+    "Ep31SvFAetugFPe5CGzJxt:2:Attestation numérique d'identité gouvernemental (EXP):1.0":
+      "https://raw.githubusercontent.com/MCN-ING/aries-oca-bundles/feat/qc-anig-test-oca/OCABundles/Ep31SvFAetugFPe5CGzJxt/Ep31SvFAetugFPe5CGzJxt%3A2%3AAttestation_num%C3%A9rique_didentit%C3%A9_gouvernemental_(EXP)%3A1.0/OCABundle.json",
+
   })
 );
 
